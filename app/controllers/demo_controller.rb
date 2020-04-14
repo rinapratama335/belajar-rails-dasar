58d8e78a511fbf415b4fc6e95d753be9058ffe31
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-14 16:34:37 +0700

    render template dasar di rails

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,13 @@
 class DemoController < ApplicationController
   def index
+    render "coba"
   end
 
   def coba
+    render "main"
+  end
+
+  def iseng
+    render plain: 'text text text'
   end
 end
